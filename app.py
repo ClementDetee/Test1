a3e5eafc93e07bbded7b769085505a3a0c8c5ad2
ClementDetee <clement.detee@gmail.com>
2025-03-26 15:38:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Le meilleur site d'**ouganda**")
+st.subheader("Et du reste d'afrique du sud ouest")
+st.text_input("ça vient d'où ? :")
+
 st.write('test site 1')
+st write('
